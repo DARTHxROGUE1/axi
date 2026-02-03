e2a757fb352a8c5194cc9718924c1ef5c6675b7d
DARTHxROGUE1 <aneeshvasisht812@gmail.com>
2026-02-03 23:04:21 +0530

    fix1

---
+++ src/axi_spi_top.vhd
@@ -0,0 +1,79 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity axi_spi_top is
+  port (
+    aclk          : in  std_logic;
+    aresetn       : in  std_logic;
+
+    s_axi_awaddr  : in  std_logic_vector(31 downto 0);
+    s_axi_awprot  : in  std_logic_vector(2 downto 0);
+    s_axi_awvalid : in  std_logic;
+    s_axi_awready : out std_logic;
+
+    s_axi_wdata   : in  std_logic_vector(31 downto 0);
+    s_axi_wstrb   : in  std_logic_vector(3 downto 0);
+    s_axi_wvalid  : in  std_logic;
+    s_axi_wready  : out std_logic;
+
+    s_axi_bresp   : out std_logic_vector(1 downto 0);
+    s_axi_bvalid  : out std_logic;
+    s_axi_bready  : in  std_logic;
+
+    s_axi_araddr  : in  std_logic_vector(31 downto 0);
+    s_axi_arprot  : in  std_logic_vector(2 downto 0);
+    s_axi_arvalid : in  std_logic;
+    s_axi_arready : out std_logic;
+
+    s_axi_rdata   : out std_logic_vector(31 downto 0);
+    s_axi_rresp   : out std_logic_vector(1 downto 0);
+    s_axi_rvalid  : out std_logic;
+    s_axi_rready  : in  std_logic;
+
+    spi_sclk      : out std_logic;
+    spi_mosi      : out std_logic;
+    spi_miso      : in  std_logic;
+    spi_ss        : out std_logic
+  );
+end entity axi_spi_top;
+
+architecture rtl of axi_spi_top is
+begin
+
+  u_axi_spi_simple : entity work.axi_spi_simple 
+    port map (
+      aclk          => aclk,
+      aresetn       => aresetn,
+
+      s_axi_awaddr  => s_axi_awaddr,
+      s_axi_awprot  => s_axi_awprot,
+      s_axi_awvalid => s_axi_awvalid,
+      s_axi_awready => s_axi_awready,
+
+      s_axi_wdata   => s_axi_wdata,
+      s_axi_wstrb   => s_axi_wstrb,
+      s_axi_wvalid  => s_axi_wvalid,
+      s_axi_wready  => s_axi_wready,
+
+      s_axi_bresp   => s_axi_bresp,
+      s_axi_bvalid  => s_axi_bvalid,
+      s_axi_bready  => s_axi_bready,
+
+      s_axi_araddr  => s_axi_araddr,
+      s_axi_arprot  => s_axi_arprot,
+      s_axi_arvalid => s_axi_arvalid,
+      s_axi_arready => s_axi_arready,
+
+      s_axi_rdata   => s_axi_rdata,
+      s_axi_rresp   => s_axi_rresp,
+      s_axi_rvalid  => s_axi_rvalid,
+      s_axi_rready  => s_axi_rready,
+
+      spi_sclk      => spi_sclk,
+      spi_mosi      => spi_mosi,
+      spi_miso      => spi_miso,
+      spi_ss        => spi_ss
+    );
+
+end architecture rtl;
