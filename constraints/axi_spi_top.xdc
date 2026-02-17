a15e8ea4a1bbd53d929bdef53b731cb1d468abd2
DARTHxROGUE1 <aneeshvasisht812@gmail.com>
2026-02-17 23:39:06 +0530

    fix2

--- constraints/axi_spi_top.xdc
+++ constraints/axi_spi_top.xdc
@@ -1,33 +1,41 @@
-set_property PACKAGE_PIN L19 [get_ports aclk]
+########################################
+# AXI CLOCK FROM EXTERNAL MASTER
+########################################
+set_property PACKAGE_PIN <PIN_ACLK> [get_ports aclk]
 set_property IOSTANDARD LVCMOS33 [get_ports aclk]
-create_clock -period 10.000 -name axi_clk -waveform {0 5} [get_ports aclk]
+create_clock -period 10.000 [get_ports aclk]
 
-set_property PACKAGE_PIN M19 [get_ports aresetn]
+########################################
+# AXI RESET
+########################################
+set_property PACKAGE_PIN <PIN_RESET> [get_ports aresetn]
 set_property IOSTANDARD LVCMOS33 [get_ports aresetn]
 set_property PULLUP true [get_ports aresetn]
 
-set_property PACKAGE_PIN W18 [get_ports spi_sclk]
+########################################
+# SPI INTERFACE
+########################################
+set_property PACKAGE_PIN <PIN_SPI_SCLK> [get_ports spi_sclk]
 set_property IOSTANDARD LVCMOS33 [get_ports spi_sclk]
 
-set_property PACKAGE_PIN W19 [get_ports spi_mosi]
+set_property PACKAGE_PIN <PIN_SPI_MOSI> [get_ports spi_mosi]
 set_property IOSTANDARD LVCMOS33 [get_ports spi_mosi]
 
-set_property PACKAGE_PIN V18 [get_ports spi_miso]
+set_property PACKAGE_PIN <PIN_SPI_MISO> [get_ports spi_miso]
 set_property IOSTANDARD LVCMOS33 [get_ports spi_miso]
 
-set_property PACKAGE_PIN V19 [get_ports spi_ss]
+set_property PACKAGE_PIN <PIN_SPI_SS> [get_ports spi_ss]
 set_property IOSTANDARD LVCMOS33 [get_ports spi_ss]
 
-set_property PACKAGE_PIN U19 [get_ports spi_ssn]
+set_property PACKAGE_PIN <PIN_SPI_SSN> [get_ports spi_ssn]
 set_property IOSTANDARD LVCMOS33 [get_ports spi_ssn]
 
-set_property PACKAGE_PIN U17 [get_ports {gpo[0]}]
-set_property PACKAGE_PIN U18 [get_ports {gpo[1]}]
-set_property PACKAGE_PIN T17 [get_ports {gpo[2]}]
-set_property PACKAGE_PIN T18 [get_ports {gpo[3]}]
-set_property PACKAGE_PIN R17 [get_ports {gpo[4]}]
-set_property PACKAGE_PIN R18 [get_ports {gpo[5]}]
-set_property PACKAGE_PIN P17 [get_ports {gpo[6]}]
-set_property PACKAGE_PIN P18 [get_ports {gpo[7]}]
+########################################
+# GPIO 
+########################################
+set_property PACKAGE_PIN <PIN_GPO0> [get_ports {gpo[0]}]
+set_property PACKAGE_PIN <PIN_GPO1> [get_ports {gpo[1]}]
+set_property PACKAGE_PIN <PIN_GPO2> [get_ports {gpo[2]}]
+set_property PACKAGE_PIN <PIN_GPO3> [get_ports {gpo[3]}]
 
 set_property IOSTANDARD LVCMOS33 [get_ports {gpo[*]}]
--- src/axi_spi_top.vhd
+++ src/axi_spi_top.vhd
@@ -1,49 +1,51 @@
--- optimized using chatgpt 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity axi_spi_top is
   port (
-    aclk          : in  std_logic;
-    aresetn       : in  std_logic;
+    -- physical FPGA pins
+    aclk     : in  std_logic;
+    aresetn  : in  std_logic;
 
-    saxi_awaddr   : in  std_logic_vector(31 downto 0);
-    saxi_awprot   : in  std_logic_vector(2 downto 0);
-    saxi_awvalid  : in  std_logic;
-    saxi_awready  : out std_logic;
+    spi_mosi : out std_logic;
+    spi_miso : in  std_logic;
+    spi_sclk : out std_logic;
+    spi_ss   : out std_logic;
+    spi_ssn  : out std_logic;
 
-    saxi_wdata    : in  std_logic_vector(31 downto 0);
-    saxi_wstrb    : in  std_logic_vector(3 downto 0);
-    saxi_wvalid   : in  std_logic;
-    saxi_wready   : out std_logic;
-
-    saxi_bresp    : out std_logic_vector(1 downto 0);
-    saxi_bvalid   : out std_logic;
-    saxi_bready   : in  std_logic;
-
-    saxi_araddr   : in  std_logic_vector(31 downto 0);
-    saxi_arprot   : in  std_logic_vector(2 downto 0);
-    saxi_arvalid  : in  std_logic;
-    saxi_arready  : out std_logic;
-
-    saxi_rdata    : out std_logic_vector(31 downto 0);
-    saxi_rresp    : out std_logic_vector(1 downto 0);
-    saxi_rvalid   : out std_logic;
-    saxi_rready   : in  std_logic;
-
-    spi_mosi      : out std_logic;
-    spi_miso      : in  std_logic;
-    spi_sclk      : out std_logic;
-    spi_ss        : out std_logic;
-    spi_ssn       : out std_logic;
-
-    gpo           : out std_logic_vector(31 downto 0)
+    gpo      : out std_logic_vector(31 downto 0)
   );
 end entity axi_spi_top;
 
 architecture rtl of axi_spi_top is
+
+  signal saxi_awaddr  : std_logic_vector(31 downto 0) := (others => '0');
+  signal saxi_awprot  : std_logic_vector(2 downto 0)  := (others => '0');
+  signal saxi_awvalid : std_logic := '0';
+  signal saxi_awready : std_logic;
+
+  signal saxi_wdata   : std_logic_vector(31 downto 0) := (others => '0');
+  signal saxi_wstrb   : std_logic_vector(3 downto 0)  := (others => '0');
+  signal saxi_wvalid  : std_logic := '0';
+  signal saxi_wready  : std_logic;
+
+  signal saxi_bresp   : std_logic_vector(1 downto 0);
+  signal saxi_bvalid  : std_logic;
+  signal saxi_bready  : std_logic := '1';
+
+  signal saxi_araddr  : std_logic_vector(31 downto 0) := (others => '0');
+  signal saxi_arprot  : std_logic_vector(2 downto 0)  := (others => '0');
+  signal saxi_arvalid : std_logic := '0';
+  signal saxi_arready : std_logic;
+
+  signal saxi_rdata   : std_logic_vector(31 downto 0);
+  signal saxi_rresp   : std_logic_vector(1 downto 0);
+  signal saxi_rvalid  : std_logic;
+  signal saxi_rready  : std_logic := '1';
+
 begin
+
 
   u_axi_spi_simple : entity work.axi_spi_simple
     generic map (
@@ -54,39 +56,42 @@ begin
       SAXI_ADDR_WIDTH => 2
     )
     port map (
-      mosi          => spi_mosi,
-      miso          => spi_miso,
-      sclk          => spi_sclk,
-      ss            => spi_ss,
-      ssn           => spi_ssn,
-      gpo           => gpo,
+    
+      mosi => spi_mosi,
+      miso => spi_miso,
+      sclk => spi_sclk,
+      ss   => spi_ss,
+      ssn  => spi_ssn,
+      gpo  => gpo,
 
-      S_AXI_ACLK    => aclk,
-      saxi_aresetn  => aresetn,
+ 
+      S_AXI_ACLK   => aclk,
+      saxi_aresetn => aresetn,
 
-      saxi_awaddr   => saxi_awaddr,
-      saxi_awprot   => saxi_awprot,
-      saxi_awvalid  => saxi_awvalid,
-      saxi_awready  => saxi_awready,
+      -- internal AXI bus
+      saxi_awaddr  => saxi_awaddr,
+      saxi_awprot  => saxi_awprot,
+      saxi_awvalid => saxi_awvalid,
+      saxi_awready => saxi_awready,
 
-      saxi_wdata    => saxi_wdata,
-      saxi_wstrb    => saxi_wstrb,
-      saxi_wvalid   => saxi_wvalid,
-      saxi_wready   => saxi_wready,
+      saxi_wdata   => saxi_wdata,
+      saxi_wstrb   => saxi_wstrb,
+      saxi_wvalid  => saxi_wvalid,
+      saxi_wready  => saxi_wready,
 
-      saxi_bresp    => saxi_bresp,
-      saxi_bvalid   => saxi_bvalid,
-      saxi_bready   => saxi_bready,
+      saxi_bresp   => saxi_bresp,
+      saxi_bvalid  => saxi_bvalid,
+      saxi_bready  => saxi_bready,
 
-      saxi_araddr   => saxi_araddr,
-      saxi_arprot   => saxi_arprot,
-      saxi_arvalid  => saxi_arvalid,
-      saxi_arready  => saxi_arready,
+      saxi_araddr  => saxi_araddr,
+      saxi_arprot  => saxi_arprot,
+      saxi_arvalid => saxi_arvalid,
+      saxi_arready => saxi_arready,
 
-      saxi_rdata    => saxi_rdata,
-      saxi_rresp    => saxi_rresp,
-      saxi_rvalid   => saxi_rvalid,
-      saxi_rready   => saxi_rready
+      saxi_rdata   => saxi_rdata,
+      saxi_rresp   => saxi_rresp,
+      saxi_rvalid  => saxi_rvalid,
+      saxi_rready  => saxi_rready
     );
 
 end architecture rtl;
